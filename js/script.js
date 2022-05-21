62f76b64df8a10ed67d7bd99b0abfc83c0cd3a50
contatofabioos@gmail.com <contatofabioos@gmail.com>
2022-05-21 11:35:36 -0300

    site game mania

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
-function mostrarMenu() {
+
+$(document).ready(function(){
+    
+    let slideAtual = 1;  
+    let listaSlides = ["banner-cs", "banner-lol", "banner-fifa" ]
+
+    setInterval(mudarSlide, 1500)
+    
+    function mudarSlide(){
+        console.log("Slide atual:", slideAtual);
+
+        //remove o slide Anterior
+        if (slideAtual > 0){
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        }else if ( slideAtual == 0){
+            $("#carrossel").removeClass(listaSlides[(listaSlides.length) - 1])
+        }
+
+        //Adiciona o slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual]);
+
+        //Indica o slide atual
+        slideAtual++
+
+        if(slideAtual > (listaSlides.length) - 1){
+            slideAtual = 0;
+        }
+
+    }
+})
+
+
+function mostrarMenu(){
     let menu = document.getElementById("menu")
+
+    if(getComputedStyle(menu).display == 'none'){
+        menu.style.display = 'flex';
+    } else{
+        menu.style.display = 'none';
+    }
+
 }
+function cadastrarNewsletter() {
+    let email = document.getElementById("campo-email").value
+
+    alert(email)
+    console.log(email)
+}
+    
